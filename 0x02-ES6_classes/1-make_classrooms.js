d2127fed2b89327ecb7e3eaa2806bcad3da6aa66
The-alpha-male <ronnymunene@outlook.com>
2024-04-26 01:18:27 +0300

    fixed code

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,14 +1,8 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-    const classes = [];
-    const classroom1 = new ClassRoom(19);
-    const classroom2 = new ClassRoom(20);
-    const classroom3 = new ClassRoom(34);
-
-    classes.push(classroom1);
-    classes.push(classroom2);
-    classes.push(classroom3);
-
-    return classes;
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+  return [room1, room2, room3]; //eslint-disable-line
 }
