3d577e87e1a99c49110c94eea9850e56be28b910
The-alpha-male <ronnymunene@outlook.com>
2024-04-29 16:29:48 +0300

    Added initial student list with basic details

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,18 @@
+export default function getListStudents() {
+    return [{
+            id: 1,
+            firstame: 'Guillaume',
+            location: 'San Franciso'
+        },
+        {
+            id: 2,
+            firstame: 'James',
+            location: 'Columbia'
+        },
+        {
+            id: 3,
+            firstame: 'Serena',
+            location: 'San Francisco'
+        },
+    ];
+}
