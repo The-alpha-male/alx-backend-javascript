b43d6bab93c3fe3e029d64ace8ce607698dca6bb
The-alpha-male <ronnymunene@outlook.com>
2024-04-30 18:31:01 +0300

    used the filter method

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,8 +1,6 @@
-function getStudentsByLocation(array, city) {
+export default function getStudentsByLocation(array, city) {
     if (!Array.isArray(array)) {
         return [];
     }
     return array.filter((obj) => obj.location === city);
-};
-
-console.log(getStudentsByLocation(students, 'San Francisco'));
+}
