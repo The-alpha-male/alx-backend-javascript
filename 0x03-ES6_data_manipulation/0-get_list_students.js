5cdf7a3a251a97a37f8875df4931365973f4b1e2
The-alpha-male <ronnymunene@outlook.com>
2024-04-30 13:47:06 +0300

    function that returns a llist of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,7 @@
-var students = [
-    { id: 1, firstName: "Guillaume", location: "San Francisco" },
-    { id: 2, firstName: "James", location: "Columbia" },
-    { id: 3, firstName: "Serena", location: "San Francisco" },
-
-]
-export async function getListStudents() {
-    const studentsToreturn = students
-        /* console.log(studentsToreturn) */
-
-    return studentsToreturn
+export default function getListStudents() {
+    return [
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+        { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    ];
 }
