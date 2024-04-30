fa58c568d80465cf1f1aa4d66556308dc1e75c11
The-alpha-male <ronnymunene@outlook.com>
2024-04-30 17:36:28 +0300

    updated code

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,18 +1,12 @@
-const getListStudents = () => [{
-        id: 1,
-        firstName: 'Guillaume',
-        location: 'San Francisco',
-    },
-    {
-        id: 2,
-        firstName: 'James',
-        location: 'Columbia',
-    },
-    {
-        id: 5,
-        firstName: 'Serena',
-        location: 'San Francisco',
-    },
-];
+/* eslint-disable */
+
+const getListStudents = () => {
+  let students = [
+    { id: 1, firstName: "Guillaume", location: "San Francisco" },
+    { id: 2, firstName: "James", location: "Columbia" },
+    { id: 5, firstName: "Serena", location: "San Francisco" },
+  ];
+  return students;
+};
 
 export default getListStudents;
