a04173efcf8ecf90e5674f0910c2eded9e27aef9
The-alpha-male <ronnymunene@outlook.com>
2024-05-01 03:20:48 +0300

    created a function that returns a map of groceries

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,10 @@
 export default function groceriesList() {
-    const map = new Map();
+  const map = new Map();
 
-    return map
-        .set('Apples', 10)
-        .set('Tomatoes', 10)
-        .set('Pasta', 1)
-        .set('Rice', 1)
-        .set('Banana', 5);
+  return map
+    .set('Apples', 10)
+    .set('Tomatoes', 10)
+    .set('Pasta', 1)
+    .set('Rice', 1)
+    .set('Banana', 5);
 }
